da646d87a1996749b75e94fcd10cd8cfeed5206e
Arun Bhadouriya <66020141+arun-bhadouriya@users.noreply.github.com>
2023-04-01 17:42:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const inputFields = document.getElementsByTagName("input");
+const btn = document.getElementsByTagName("button");
+
+console.log(inputFields[0])
+console.log(btn);
+btn[0].addEventListener('click',(e)=>{
+	const fname = inputFields[0].value;
+	const lname = inputFields[1].value;
+	const phn = inputFields[2].value;
+	const email = inputFields[3].value;
+	window.alert(`First Name: ${fname}
+Last Name: ${lname}
+Phone Number: ${phn}
+Email ID: ${email}`);
+})
